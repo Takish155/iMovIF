3f75594706d7dd5f45da418943ebbf9a1fe1ab73
takish155 <rimutakeshi155@gmail.com>
2023-10-22 22:21:09 +0900

    added footer for TMDB

---
+++ src/pages/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className="text-center my-10 text-2xl">
+        This product uses the TMDB API but is not endorsed or certified by TMDB.
+      </p>
+    </footer>
+  );
+}
